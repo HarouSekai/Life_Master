e1328aacee3ad8575649606cd89527ac183c6fbc
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-30 23:09:17 +0900

    indexアクションの内容を定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+
   def index
+    @articles = Article.order("DESC updated_at")
   end
 
   def new
