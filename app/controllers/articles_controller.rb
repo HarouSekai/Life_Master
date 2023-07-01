74a26d64b1ece9aad161efee4a3e1347917a8ac1
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-01 10:16:02 +0900

    newアクション、createアクションを定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,26 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @articles = Article.order("DESC updated_at")
+    @articles = Article.order("updated_at DESC")
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :summary).merge(user_id: current_user.id)
   end
 end
