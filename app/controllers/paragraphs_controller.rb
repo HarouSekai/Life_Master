2a10d092ebf0cb97db6428f26feb2e8d054e1281
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-01 17:16:04 +0900

    newアクションの内容、createアクションを定義

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -1,4 +1,18 @@
 class ParagraphsController < ApplicationController
+  before_action :find_user_article
+
   def new
+    @paragraph = Paragraph.new
   end
+
+  def create
+  end
+
+  private
+
+  def find_user_article
+    @user = params[:user_id]
+    @article = params[:article_id]
+  end
+
 end
