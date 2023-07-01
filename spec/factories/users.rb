a486ce16396b8099371088307d84f0da76599718
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-01 16:11:45 +0900

    Userモデル単体テストコードでパスワードの条件を変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password }
+    password { 'a1' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
   end
 end
