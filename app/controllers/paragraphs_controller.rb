d12f52521973bfaea9e721ba0b5e6a301bfd8d2f
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-01 23:05:16 +0900

    ユーザー情報によるアクセス制限

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -1,5 +1,7 @@
 class ParagraphsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_user_article
+  before_action :move_to_show
 
   def new
     @paragraph = Paragraph.new
@@ -8,6 +10,7 @@ class ParagraphsController < ApplicationController
   def create
     @paragraph = Paragraph.new(paragraph_params)
     if @paragraph.save
+      flash[:notice] = "段落が保存されました。"
       redirect_to edit_user_article_path(@user, @article)
     else
       render :new
@@ -23,5 +26,10 @@ class ParagraphsController < ApplicationController
 
   def paragraph_params
     params.permit(:headline, :content, :article_id)
+  end
+
+  def move_to_show
+    return unless current_user.id != params[:user_id].to_i
+    redirect_to user_article_path(params[:user_id], params[:article_id])
   end
 end
