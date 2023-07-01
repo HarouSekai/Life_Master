62303fe9c773cb5994b4d5b6483c8ed1ac613ada
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-01 13:37:11 +0900

    リンクの変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :find_article, only: [:show]
+  before_action :find_user, only: [:index, :show, :new, :create]
 
   def index
     @articles = Article.order("updated_at DESC")
@@ -17,13 +18,19 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to articles_path
+      redirect_to root_path
     else
       render :new
     end
   end
 
   private
+
+  def find_user
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    end
+  end
 
   def find_article
     @article = Article.find(params[:id])
