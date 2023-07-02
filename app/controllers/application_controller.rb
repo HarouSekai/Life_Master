3726ef70d1c9e39f1081b8ffd9595eb63a3edce5
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-02 16:12:21 +0900

    BASIC認証を廃止

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["LIFE_MASTER_AUTH_USER"] && password == ENV["LIFE_MASTER_AUTH_PASSWORD"]
-    end
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
