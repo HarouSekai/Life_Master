81783b323652bfb98355fd161d0ab568a80471e8
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-03 16:43:50 +0900

    paragraphs#destroyへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [] do
     resources :articles do
-      resources :paragraphs, only: [:new, :create, :edit, :update]
+      resources :paragraphs, only: [:new, :create, :edit, :update, :destroy]
     end
   end
 end
