ae3955f0dc81baabe23f8590d731f4ff5eb211a5
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-03 17:17:23 +0900

    imagesコントローラーへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [] do
     resources :articles do
-      resources :paragraphs, only: [:new, :create, :edit, :update, :destroy]
+      resources :paragraphs, only: [:new, :create, :edit, :update, :destroy] do
+        resources :images, only: [:new, :create]
+      end
     end
   end
 end
