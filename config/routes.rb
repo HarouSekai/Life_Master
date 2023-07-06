7c3122ae9a966b7e175cc924371ee642b6167f9b
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-06 22:07:59 +0900

    usersコントローラーへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/top', to: 'home#top'
   root to: 'articles#index'
   devise_for :users
-  resources :users, only: [] do
+  resources :users, only: [:show] do
     resources :articles do
       resources :paragraphs, only: [:new, :create, :edit, :update, :destroy] do
         resources :images, only: [:new, :create, :edit, :update, :destroy]
