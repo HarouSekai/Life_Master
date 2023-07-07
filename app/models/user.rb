3c9b27b359f887d54f30513dbe0325825d0a38f9
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-07 23:15:41 +0900

    UserモデルにAvatarモデルとのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :avatar, dependent: :destroy
   has_many :articles, dependent: :destroy
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
