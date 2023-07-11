ea6b1111b833caeec1f761c42f0d46a522a75199
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-11 20:51:48 +0900

    avatarsコントローラーのeditアクション、updateアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'home/top', to: 'home#top'
   root to: 'articles#index'
   devise_for :users
+  get 'users/:user_id/avatar/edit', to: 'avatars#edit'
+  put 'users/:user_id/avatar', to: 'avatars#update'
   resources :users, only: [:show] do
     resources :articles do
       resources :paragraphs, only: [:new, :create, :edit, :update, :destroy] do
