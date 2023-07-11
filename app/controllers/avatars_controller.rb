d09568a78998fda250899553c381f9ae0994c3ca
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-11 22:08:04 +0900

    アバター作成処理を実装

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,7 +1,25 @@
 class AvatarsController < ApplicationController
+  before_action :find_user_avatar
+
   def edit
   end
 
   def update
+    if @user.avatar == nil
+      @avatar = Avatar.new(avatar_params)
+      @avatar.save
+    end
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def find_user_avatar
+    @user = User.find(current_user.id)
+    @user.avatar == nil ? @avatar = Avatar.new : @avatar = Avatar.find(@user.avatar.id)
+  end
+
+  def avatar_params
+    params.permit(:avatar, :user_id)
   end
 end
