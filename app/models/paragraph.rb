7024054f61344db27074bbfd5ead7da2095a6c0b
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-19 23:01:45 +0900

    段落作成・編集機能のエラーメッセージを日本語化

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -1,5 +1,5 @@
 class Paragraph < ApplicationRecord
   belongs_to :article
   has_many :images, dependent: :destroy
-  validates :article_id, presence: true
+  validates :article_id, presence: {message: 'を作成してください'}
 end
