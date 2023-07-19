ed2f17eb122272bee1a3b917b976180231d92940
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-07-19 22:42:47 +0900

    Articleモデルのuser_idのエラーを日本語化

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :paragraphs, dependent: :destroy
 
-  validates :title, :summary, :user_id, presence: true
+  validates :title, :summary, presence: true
+  validates :user_id, presence: {message: 'を新規登録してください'}
 end
